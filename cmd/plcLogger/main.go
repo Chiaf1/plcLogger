d7e8a2e194cf5669d0e95130d644ceccdd716d23
Daniele <28843028+Chiaf1@users.noreply.github.com>
2026-02-25 11:09:30 +0100

    updated plcComunication for new logic
    Aggiornata plccomunication secondo la nuova logica dell'app, ovvero con una strutture thread safe che contiene i valori aggiorni delle tag del plc che le altre sezioni di programma possono interrogare in seicurezza lavorando su elle copie

--- cmd/plcLogger/main.go
+++ cmd/plcLogger/main.go
@@ -1,46 +1,38 @@
 package main
 
-import (
-	"log"
-
-	"github.com/chiaf1/plclogger/internal/config"
-	datastorage "github.com/chiaf1/plclogger/internal/dataStorage"
-	"github.com/chiaf1/plclogger/internal/logger"
-	plccomunication "github.com/chiaf1/plclogger/internal/plcComunication"
-)
-
 const CONFIG_PATH = "./config.yaml"
 const LAST_VALUES_PATH = "./data/last_values.json"
 const ON_CHANGE_LOG_PATH = "./log/onChange.log"
 const PERIODIC_LOG_PATH = "./log/periodic.log"
 
 func main() {
-	//loading config
-	var conf config.Config
-	err := conf.Load(CONFIG_PATH)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Config loaded")
+	/*
+		//loading config
+		var conf config.Config
+		err := conf.Load(CONFIG_PATH)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Config loaded")
 
-	//loading last values
-	var lv datastorage.LastValues
-	err = lv.LoadLastValues(LAST_VALUES_PATH)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Last values loaded")
+		//loading last values
+		var lv datastorage.LastValues
+		err = lv.LoadLastValues(LAST_VALUES_PATH)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Last values loaded")
 
-	//retreving the current values and checking if they changed
-	var rv plccomunication.ReadValues
-	err = rv.UpdateCurrentVals(conf.DataToLog, conf.Connection)
-	if err != nil {
-		log.Fatal(err)
-	}
-	logger.CheckChangedValues(lv, rv.GetOnChange(), LAST_VALUES_PATH, ON_CHANGE_LOG_PATH)
-	log.Println("OnChange logged")
+		//retreving the current values and checking if they changed
+		var rv plccomunication.ReadValues
+		err = rv.UpdateCurrentVals(conf.DataToLog, conf.Connection)
+		if err != nil {
+			log.Fatal(err)
+		}
+		logger.CheckChangedValues(lv, rv.GetOnChange(), LAST_VALUES_PATH, ON_CHANGE_LOG_PATH)
+		log.Println("OnChange logged")
 
-	logger.LogPeriodic(rv.GetPeriodic(), PERIODIC_LOG_PATH)
-	log.Println("Periodic logged")
-
+		logger.LogPeriodic(rv.GetPeriodic(), PERIODIC_LOG_PATH)
+		log.Println("Periodic logged")
+	*/
 }
--- internal/plcComunication/plcComunication.go
+++ internal/plcComunication/plcComunication.go
@@ -2,14 +2,19 @@ package plccomunication
 
 import (
 	"fmt"
+	"maps"
+	"sync"
 
 	"github.com/chiaf1/plclogger/internal/domain"
-	"github.com/chiaf1/plclogger/internal/logger"
-	plcdrivers "github.com/chiaf1/plclogger/internal/plcDrivers"
 )
 
-// ReadValues type is a slice of Value that rapresent a tag read from the PLC with flags related to the type of log they belong to
-type ReadValues []Value
+// ReadValues is a struct containing a map to all the Values of the tags that are read from the PLC
+// this struct also has a mutex to access the data safely from different threads
+// the value map has the key value queal to the name of the tag
+type ReadValues struct {
+	mu   sync.RWMutex
+	vals map[string]Value
+}
 
 // Value type that raperesent one tag with the flags PeriodicLog, OnChangeLog, ShowDashboard to rapresent the log they belong to
 type Value struct {
@@ -20,92 +25,75 @@ type Value struct {
 	ShowDashboard bool
 }
 
+// NewReadValues is a constructor for ReadValues struct
+func NewReadValues() *ReadValues {
+	return &ReadValues{vals: make(map[string]Value)}
+}
+
+// UpdateVal is used to safely update the value of a tag or add it if not present
+func (rv *ReadValues) UpdateVal(v Value) error {
+	if v.Name == "" {
+		return fmt.Errorf("tag with no name can't be added/updated")
+	}
+	rv.mu.Lock()
+	rv.vals[v.Name] = v
+	rv.mu.Unlock()
+	return nil
+}
+
 // UpdateCurrentVals connectes to the plc, retrieves the current values of all the tags of the slice tags and stores them in the struct
-// ReadValues. It read all the values from the PLC then you can filter them based on what you need
-func (rv *ReadValues) UpdateCurrentVals(tags []domain.Tag, conf domain.ConnectionConfig) error {
+// ReadValues. It updates the structure ReadValues in thread safe way
+func (rv *ReadValues) UpdateCurrentVals(tags []domain.Tag, plc domain.PLCDriver) error {
 	if len(tags) <= 0 {
 		return fmt.Errorf("error no tags to read")
 	}
 
-	client := plcdrivers.NewS7Client(conf)
-
-	err := client.Connect()
-	if err != nil {
-		return fmt.Errorf("error connecting to plc client: %w", err)
-	}
-	defer client.Disconnect()
-
+	// We'll work on a temp structure so that we can lock the real one only after we have read all the tags
+	tmp := make(map[string]Value, len(tags))
+	// we create a slice to store all the errors during the reading of the tags
 	var errs []error
 	for _, t := range tags {
-		val, err := client.Read(t.PlcTag)
+		val, err := plc.Read(t.PlcTag)
 		if err != nil {
 			errs = append(errs, fmt.Errorf("error reading tag %s from plc: %w", t.Name, err))
 			continue
 		}
-		newVal := Value{
+		tmp[t.Name] = Value{
 			Name:          t.Name,
 			Val:           val,
 			PeriodicLog:   t.PeriodicLog,
 			OnChangeLog:   t.OnChangeLog,
 			ShowDashboard: t.ShowDashboard,
 		}
-		err = rv.UpdateVal(newVal)
-		if err != nil {
-			errs = append(errs, fmt.Errorf("error updating tag value: %w", err))
-		}
 	}
+
+	// now we'll update the real structure
+	rv.mu.Lock()
+	maps.Copy(rv.vals, tmp)
+	rv.mu.Unlock()
+
 	if len(errs) > 0 {
 		return fmt.Errorf("read completed with %d errors: %v", len(errs), errs)
 	}
 	return nil
 }
 
-// UpdateVal updates the value if it exist, if not it appends it
-func (rv *ReadValues) UpdateVal(v Value) error {
-	if v.Name == "" {
-		return fmt.Errorf("tag with no name can't be added/updated")
+// Snapshot safely retrives a copy/snapshot of the current state of the values stored in the ReadValue structure
+func (rv *ReadValues) SnapShot() []Value {
+	rv.mu.RLock()
+	defer rv.mu.RUnlock()
+
+	out := make([]Value, 0, len(rv.vals))
+	for _, v := range rv.vals {
+		out = append(out, v)
 	}
-	if len(*rv) > 0 {
-		for i, p := range *rv {
-			if p.Name == v.Name {
-				(*rv)[i] = v
-				return nil
-			}
-		}
-	}
-	*rv = append(*rv, v)
-	return nil
+	return out
 }
 
-// GetPeriodic from the ReadVals slice retrive only the vals to log periodically
-func (rv ReadValues) GetPeriodic() logger.CurrentValues {
-	LogTags := make(logger.CurrentValues)
-	for _, v := range rv {
-		if v.PeriodicLog {
-			LogTags[v.Name] = v.Val
-		}
-	}
-	return LogTags
-}
-
-// GetOnChange from the ReadVals slice retrive only the vals to log on value change
-func (rv ReadValues) GetOnChange() logger.CurrentValues {
-	LogTags := make(logger.CurrentValues)
-	for _, v := range rv {
-		if v.OnChangeLog {
-			LogTags[v.Name] = v.Val
-		}
-	}
-	return LogTags
-}
-
-// GetShowDashboard from the ReadVals slice retrive only the vals to show on the dashboard
-func (rv ReadValues) GetShowDashboard() logger.CurrentValues {
-	LogTags := make(logger.CurrentValues)
-	for _, v := range rv {
-		if v.ShowDashboard {
-			LogTags[v.Name] = v.Val
-		}
-	}
-	return LogTags
+// GetByName safely retrives the value of a single tag based on the name
+func (rv *ReadValues) GetByName(name string) (Value, bool) {
+	rv.mu.RLock()
+	v, ok := rv.vals[name]
+	rv.mu.RUnlock()
+	return v, ok
 }
